e31c2c4543960f7aba4497faeaa071811770adae
Varun <sharma.varun76@yahoo.in>
2022-08-17 13:53:55 +0530

    Added: Refresh token

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,24 +25,52 @@ exports.login = async (req, res) => {
                 message: errorMessages.INVALID_EMAIL_PASSWORD
             })
         }
-
+        // token
         const token = jwt.sign({
             subject: user.email,
             // issuer: process.env.JWT_ISSUER,
             // audience: process.env.JWT_CONSUMER,
             algorithm:  "RS256"
         }, process.env.JWT_SECRET, { expiresIn: 60 * 60 })
+        // refresh token
+        const refreshToken = jwt.sign({
+            subject: user.email,
+            algorithm:  "RS256"
+        }, process.env.JWT_REFRESH_SECRET, { expiresIn: 60 * 60 * 60})
 
-        await userModel.findOneAndUpdate({
-            email
-        }, {
-            $set: {
-                token
-            }
-        })
         return res.status(200).json({
             token,
+            refreshToken,
             message: 'Valid user.'
+        })
+    } catch(e) {
+        return res.status(500).json({
+            message: e.message
+        })
+    }
+}
+
+exports.refreshToken = async (req, res) => {
+    try {
+        const user = await userModel.findOne({
+            _id: req.authorized
+        })
+        // generate token
+        const token = jwt.sign({
+            subject: user.email,
+            // issuer: process.env.JWT_ISSUER,
+            // audience: process.env.JWT_CONSUMER,
+            algorithm:  "RS256"
+        }, process.env.JWT_SECRET, { expiresIn: 60 * 60 })
+        // generate refresh token
+        const refreshToken = jwt.sign({
+            subject: user.email,
+            algorithm:  "RS256"
+        }, process.env.JWT_REFRESH_SECRET, { expiresIn: 60 * 60 * 60})
+        return res.status(200).json({
+            token,
+            refreshToken,
+            message: 'Token updated succssfully.'
         })
     } catch(e) {
         return res.status(500).json({
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const { validate } = require('express-validation')
-const authValidation = require('../validations/authValidation')
-const authController = require('../controllers/authController')
+const { loginValidation } = require('../validations/authValidation')
+const {login, refreshToken } = require('../controllers/authController')
+const verifyRefreshToken = require('../middleware/verifyRefreshToken')
 const authRouter = express()
 
-authRouter.post('/', validate(authValidation.loginValidation), authController.login)
+authRouter.post('/', validate(loginValidation), login)
+authRouter.post('/refresh-token', verifyRefreshToken, refreshToken)
 
 module.exports = authRouter
