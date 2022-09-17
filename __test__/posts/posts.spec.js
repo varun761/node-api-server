42e73172c7912917199d64f481b5679b91eb91cb
Varun <sharma.varun76@yahoo.in>
2022-09-17 17:12:34 +0530

    Added: Unit testing

---
+++ __test__/posts/posts.spec.js
@@ -0,0 +1,21 @@
+const app = require('../../index')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const should = chai.should()
+
+chai.use(chaiHttp)
+
+describe('GET /v1/post/', () => {
+    it('should return all the posts', (done) => {
+        chai
+        .request(app)
+        .get('/v1/post/')
+        .end((err, response) => {
+            response.should.have.status(200)
+            response.body.should.have.keys('posts', 'total')
+            response.body.posts.should.be.a('array')
+            response.body.total.should.be.a('number')
+            done()
+        })
+    })
+})
